e15519a393981d173b6cb16e40aa1e704b679b09
Rajan Bhatia <rajanbhatia100@gmail.com>
2021-08-11 14:02:57 +0530

    indexUpdate

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
-const express= require("express");
+const express = require('express');
+
 const app = express();
 const route = express.Router();
-const config= require("./config");
+const cors = require('cors');
+const config = require('./config');
 
+const employeeRoutes = require('./routes/employee.routes');
 
+const computerRoutes = require('./routes/computers.routes');
 
-const employeeRoutes = require("../src/routes/employee.routes");
-
-const computerRoutes = require("../src/routes/computers.routes");
-
-const attendanceRoutes=require("../src/routes/attendance.routes");
-
+const attendanceRoutes = require('./routes/attendance.routes');
 
 app.use(express.json());
 
-app.use("/employee",employeeRoutes)
+app.use('/employee', employeeRoutes);
 
-app.use("/computer",computerRoutes)
+app.use('/computer', computerRoutes);
 
-app.use("/attendance",attendanceRoutes)
+app.use('/attendance', attendanceRoutes);
 
+app.use(cors());
 
- app.use(route);
+app.use(route);
 
-app.listen(3000,()=>{
-    console.log("App is listening to the port 3000");
-
-})
-
-
+app.listen(3000, () => {
+  console.log('App is listening to the port 3000');
+});
